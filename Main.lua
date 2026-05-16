2a4044b399ea04fe8b7088537ba9ec51e76387a3
Logan <ezekielquisky1z@outlook.com>
2026-05-16 15:44:07 -0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,6 +1,5 @@
 queue_on_teleport([[
     if (game.PlaceId == 10179538382) then
-        messagebox("Not console servers.", "hello", "1")
         game:GetService("ScriptContext"):SetTimeout(.5)
         local __OldNamecall = hookmetamethod(game, "__namecall", newcclosure(function(self, ...)
             local Method = getnamecallmethod()
@@ -14,7 +13,6 @@ queue_on_teleport([[
         end))
         loadstring(game:HttpGet("https://raw.githubusercontent.com/EzkieMalia/ConsoleLoader/refs/heads/main/Main.lua"))()
     elseif (game.PlaceId == 13643807539) then
-        messagebox("Is console servers.", "hello", "1")
         game:GetService("ScriptContext"):SetTimeout(.5)
         repeat task.wait() until not game.ReplicatedFirst:FindFirstChild("Intro")
         game:GetService("ScriptContext"):SetTimeout(9e9)
