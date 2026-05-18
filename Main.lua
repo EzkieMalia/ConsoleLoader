d6ba360e0f8ec8a235d7d5c73599ba5cc5f62b4d
Logan <ezekielquisky1z@outlook.com>
2026-05-18 14:12:07 -0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,27 +1,19 @@
 queue_on_teleport([[
     game:GetService("ScriptContext"):SetTimeout(.5)
     if (game.PlaceId == 10179538382) then
-        game:GetService("ScriptContext"):SetTimeout(.5)
         local __OldNamecall = nil
         __OldNamecall = hookmetamethod(game, "__namecall", newcclosure(function(self, ...)
             local Method = getnamecallmethod()
             if (Method == "IsTenFootInterface") then
                 return true
             end
-            if (Method == "InvokeServer" and tostring(self) ~= "GetInitDataRequest") then
+            if (Method ~= "FireServer") then
                 return task.wait(9e9)
-            end
-            if (Method == "FireServer") then
-                local Arguments = table.pack(...)
-                if (#Arguments) ~= 1 then
-                    return task.wait(9e9)
-                end
             end
             return __OldNamecall(self, ...)
         end))
         loadstring(game:HttpGet("https://raw.githubusercontent.com/EzkieMalia/ConsoleLoader/refs/heads/main/Main.lua"))()
     elseif (game.PlaceId == 13643807539) then
-        game:GetService("ScriptContext"):SetTimeout(.5)
         repeat task.wait() until not game.ReplicatedFirst:FindFirstChild("Intro")
         game:GetService("ScriptContext"):SetTimeout(9e9)
     end
